11385abdd815887d4bfacfb22b1830cff7431807
systopia <endres@systopia.de>
2016-03-07 13:16:03 +0100

    proposed fix for https://github.com/systopia/de.systopia.campaign/issues/14

--- js/campaign.js
+++ js/campaign.js
@@ -352,7 +352,8 @@
         var pie = d3.layout.pie().value(function(d){return d.value;});
         var arc = d3.svg.arc().outerRadius(r);
 
-        var arcs = vis.selectAll("g.slice").data(pie).enter().append("svg:g").attr("class", "slice");
+        var enter = vis.selectAll("g.slice").data(pie).enter();
+        var arcs  = enter.insert("svg:g",":first-child").attr("class", "slice");
         arcs.append("svg:path")
             .attr("fill", function(d, i){
                 return color(i);
@@ -361,13 +362,14 @@
                 return arc(d);
             });
 
-        // add text
-        arcs.append("svg:text").attr("transform", function(d){
+        // add label
+        var labels = enter.append("svg:g").attr("class", "slice");
+        labels.append("svg:text").attr("transform", function(d){
         			d.innerRadius = 0;
         			d.outerRadius = r;
-            return "translate(" + arc.centroid(d) + ")";}).attr("text-anchor", "middle").text( function(d, i) {
-              return data[i].label;}
-        		);
+            return "translate(" + arc.centroid(d) + ")";})
+                   .attr("text-anchor", "middle")
+                   .text( function(d, i) {return data[i].label;} );
       }
     }
   });
