cb6b3880c981abc44ef1e1bdef15ff91a7635522
Matthew Wire <mjw@mjwconsult.co.uk>
2017-04-28 18:18:11 +0100

    #632 Fix permissions issue when viewing campaign and the user has only 'manage campaign' permission

--- campaign.php
+++ campaign.php
@@ -272,6 +272,7 @@ function campaign_civicrm_alterAPIPermissions($entity, $action, &$params, &$perm
   $permissions['campaign_tree']['gettree'] = array('manage campaign');
   $permissions['campaign_tree']['setnodeparent'] = array('manage campaign');
   $permissions['campaign_tree']['clone'] = array('manage campaign');
+  $permissions['campaign_tree']['getcustominfo'] = array('manage campaign');
 }
 
 /**
