c0e29a9b811b087da700cde8b3d64250aaf3a4eb
Marc Michalsky <m.michalsky@posteo.de>
2024-01-30 13:17:29 +0100

    fix #118: pass parameter names instead of integer
    Passing an array of parameter names to CRM_Utils_Hook::invoke() instead of an integer. See https://github.com/civicrm/civicrm-core/commit/ecb0ae5d0d607a53da2037f7fc618c38e23b81db

--- CRM/Utils/CampaignCustomisationHooks.php
+++ CRM/Utils/CampaignCustomisationHooks.php
@@ -17,7 +17,7 @@ class CRM_Utils_CampaignCustomisationHooks {
   static $null = NULL;
 
   static function campaign_kpis($campaign_id, &$kpi_list, $level) {
-    return CRM_Utils_Hook::singleton()->invoke(3, $campaign_id, $kpi_list, $level, self::$null, self::$null, self::$null, 'civicrm_campaignKpis');
+    return CRM_Utils_Hook::singleton()->invoke(["campaign_id", "kpi_list", "level"], $campaign_id, $kpi_list, $level, self::$null, self::$null, self::$null, 'civicrm_campaignKpis');
   }
 
 }
